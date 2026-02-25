766c85497d7fe6e2e64a768897a14c7ab7e392d5
ramadanny <klopisprem@gmail.com>
2026-02-25 09:53:38 +0700

    release

--- plugins/info/info-ping.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m) => {
-    const start = performance.now();
-    const end = performance.now();
-    const resp = (end - start).toFixed(4);
-    m.reply(`${resp} ms`);
-};
-handler.command = /^(ping)$/i;
-export default handler;
